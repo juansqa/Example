b347da5009524b6f0fd5b4f03bfddce2de85d82d
JCMR <juanc.munoz@sqasa.co>
2019-10-23 18:29:36 -0500

    mi primer commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+//		int num1= 10;
+//		int num2= 5;
+		
+//		System.out.println("El resultado de la suma es: " + (num1+num2));
+//		System.out.println("El resultado de la resta es: " + (num1-num2));
+//		System.out.println("El resultado de la multiplicación es: " + (num1*num2));
+//		System.out.println("El resultado de la división es: " + (num1/num2));
+
+//		if(num1>num2) {
+//			System.out.println("el numero " + num1 + " es mayor que el numero " + num2 );
+//			
+//		}
+		System.out.println("ingrese un numero: ");
+		int num1=sc.nextInt();
+		System.out.println("ingrese un numero: ");
+		int num2=sc.nextInt();
+//		System.out.println("El resultado de la suma es: " + (num1+num2));
+		
+//		for(int i=0;i<=100;i++) {
+//			System.out.println("los numeros son: " + i);
+//		}
+		
+		Operaciones ope=new Operaciones();
+		//ope.suma(num1,num2);
+		ope.entreValores(num1, num2);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
---
+++ src/Operaciones.java
@@ -0,0 +1,25 @@
+
+public class Operaciones {
+
+	public void suma(int num1, int num2) {
+//		int num1=10;
+//		int num2=5;
+		System.out.println("La suma es:" + (num1 + num2));
+
+	}
+
+	public void entreValores(int num1, int num2) {
+		for (int i = num1 + 1; i < num2; i++) {
+			System.out.println("los numeros son:" + i);
+		}
+
+	}
+
+	public void multiplicacion() {
+
+	}
+
+	public void division() {
+
+	}
+}
